ec5887f91200499474a079f6c82010f719bd4e18
Love Chandra <lakkukholiya11@gmail.com>
2024-07-23 11:22:37 +0530

    feature1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("hello Love");
-        System.out.println("git commit changes");
+        System.out.println("hi love this is git branch feature1");
     }
 }
